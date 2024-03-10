f846573237dc3d61f3028e5308b1c8492950dbba
Pimnapa Sriboonreuang <126116881+nampimnapa@users.noreply.github.com>
2024-03-11 03:03:22 +0700

    updated

--- pages/manufacture/add.tsx
+++ pages/manufacture/add.tsx
@@ -15,10 +15,15 @@ function add() {
         { id: 2, name: "ดิป" },
         { id: 3, name: "เดี่ยว" }
         ];
-    const unit =
-        [{ un_id: 1, un_name: "กรัม" },
-        { un_id: 2, un_name: "ลิตร" },
-        { un_id: 3, un_name: "สอบปุ๋ย" }
+    const pd =
+        [{ id: 1, name: "เรดเวลเวด", type: 1 },
+        { id: 2, name: "ใบเตย", type: 1 },
+        { id: 3, name: "ออริจินอล", type: 1 },
+        { id: 4, name: "นมฮอกไกโด", type: 2 },
+        { id: 5, name: "ชาเขียว", type: 2 },
+        { id: 6, name: "ช็อค", type: 2 },
+        { id: 7, name: "บอมโบโลนี", type: 3 },
+        { id: 8, name: "บานอฟฟี่", type: 3 },
         ];
 
     return (
@@ -27,57 +32,59 @@ function add() {
             <form>
                 <div className="grid grid-cols-4">
                     <div className="flex items-center justify-center">
-                        <p className="text-sm px-6 py-2 text-[#73664B] flex justify-center items-center">วัตถุดิบ:</p>
+                        <p className="text-sm px-6 py-2 text-[#73664B] flex justify-center items-center">ประเภทสินค้า:</p>
                         <select id="ingredients"
                             className="bg-[#E3D8BF] w-full block rounded-md border py-1 text-[#73664B] shadow-sm sm:text-sm sm:leading-6">
-                            {ind.map((ind) => (
-                                <option key={ind.ind_id} value={ind.ind_id}>
-                                    {ind.ind_name}
+                            {product_type.map((pd) => (
+                                <option key={pd.id} value={pd.id}>
+                                    {pd.name}
                                 </option>
                             ))}
                         </select>
                     </div>
                     <div className=" flex items-center justify-center">
-                        <p className="text-sm px-5 py-2 text-[#73664B] flex justify-center ">ปริมาณ:</p>
-                        <input
-                            min="1"
-                            type="number"
-                            name="count"
-                            id="count"
-                            className="px-3 bg-[#FFFFDD] w-1/2 block rounded-t-md border border-b-[#C5B182] py-1 text-[#C5B182] shadow-sm  placeholder:text-[#C5B182]  sm:text-sm sm:leading-6 focus:outline-none"
-                        />
-                    </div>
-                    <div className=" flex items-center justify-center">
-                        <p className="text-sm px-6 py-2 text-[#73664B] flex justify-center ">หน่วย:</p>
-                        <select id="unit"
+                        <p className="text-sm px-5 py-2 text-[#73664B] flex justify-center ">สินค้า:</p>
+                        <select id="ingredients"
                             className="bg-[#E3D8BF] w-full block rounded-md border py-1 text-[#73664B] shadow-sm sm:text-sm sm:leading-6">
-                            {unit.map((unit) => (
-                                <option key={unit.un_id} value={unit.un_id}>
-                                    {unit.un_name}
+                            {product_type.map((pd) => (
+                                <option key={pd.id} value={pd.id}>
+                                    {pd.name}
                                 </option>
                             ))}
                         </select>
+                    </div>
+                    <div className=" flex items-center justify-center">
+                        <p className="text-sm px-6 py-2 text-[#73664B] flex justify-center ">จำนวน:</p>
+                        <input
+                            placeholder="จำนวน"
+                            type="text"
+                            name="qtymin"
+                            id="qtymin"
+                            autoComplete="off"
+                            className="px-3 bg-[#FFFFDD] block w-full rounded-t-md border border-b-[#C5B182] py-1.5 text-[#C5B182] shadow-sm placeholder:text-[#C5B182] sm:text-sm sm:leading-6 focus:outline-none"
+                        />
                     </div>
                     <div className="scale-75 w-1/2 h-auto flex justify-center">
                         <button
-                            onClick={handleSubmit}
+
                             type="submit"
                             value="เพิ่มวัตถุดิบ"
                             className="text-lg text-white border  bg-[#F2B461] rounded-full py-2 px-2 ">เพิ่มวัตถุดิบ</button></div>
                 </div>
             </form>
+            <p className="m-4 text-[#73664B] border-b-1 border-b-[#C5B182]">รายละเอียดใบสั่งผลิต</p>
             <div className="mx-6 mt-3">
                 <div className="flex flex-col">
                     <div className="bg-[#908362] text-white text-sm flex">
-                        <div className="flex-1 py-3 text-center">วัตถุดิบ</div>
-                        <div className="flex-1 py-3 text-center">ปริมาณ</div>
-                        <div className="flex-1 py-3 text-center">หน่วย</div>
+                        <div className="flex-1 py-3 text-center">ประเภทสินค้า</div>
+                        <div className="flex-1 py-3 text-center">สินค้า</div>
+                        <div className="flex-1 py-3 text-center">จำนวน</div>
                         <div className="flex-1 py-3 text-center"></div>
                     </div>
                     <div className="max-h-40 overflow-y-auto mb-5">
                         <table className="w-full">
                             <tbody className="w-full">
-                                {addedIngredients.slice().reverse().map((ingredient) => (
+                                {/* {addedIngredients.slice().reverse().map((ingredient) => (
                                     <tr key={ingredient.id} className="even:bg-[#F5F1E8] border-b h-10 text-sm odd:bg-white border-b h-10 text-sm flex items-center">
                                         <td scope="col" className="flex-1 text-center">{ind.find((i) => i.ind_id === parseInt(ingredient.id)).ind_name}</td>
                                         <td scope="col" className="flex-1 text-center">{ingredient.quantity}</td>
@@ -90,7 +97,7 @@ function add() {
                                             </div>
                                         </td>
                                     </tr>
-                                ))}
+                                ))} */}
                             </tbody>
                         </table>
                     </div>
